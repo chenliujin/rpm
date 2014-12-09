a5c92e803c86cf19c4bcbf77a25fbce045ffa8cf
chenliujin <liujin.chen@qq.com>
2014-12-09 16:03:48 +0800

    update

--- zencart/includes/local/configure.php
+++ zencart/includes/local/configure.php
@@ -3,3 +3,12 @@
  * 新模板
  */
 define('DIR_WS_TEMPLATE', 'includes/templates/cigabuy/'); 
+
+/**
+ * MySQL 跟踪日志
+ * queryFactory::write_log()
+ * DIR_FS_SQL_CACHE . '/sql_log_' . date("Ymd") . '.log'
+ */
+define('SQL_DEBUG', TRUE);
+define('DIR_FS_SQL_CACHE', '/ssd/www/zencart/weblogs/');
+
