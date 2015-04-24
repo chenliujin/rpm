40b692452caa10fe9ecc170b0b67ae1fbc223306
chenliujin <liujin.chen@qq.com>
2015-04-24 11:44:38 +0800

    update

---
+++ thrift/php/0.9.0/Thrift/requires.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once 'Thrift/Base/TBase.php';
+require_once 'Thrift/Exception/TException.php';
+require_once 'Thrift/Exception/TProtocolException.php';
+require_once 'Thrift/Exception/TApplicationException.php';
+require_once 'Thrift/Protocol/TProtocol.php';
+require_once 'Thrift/Protocol/TBinaryProtocol.php';
+require_once 'Thrift/Type/TType.php';
+require_once 'Thrift/Type/TMessageType.php';
