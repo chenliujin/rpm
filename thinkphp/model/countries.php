f8e10ce1fe85293aceb6a741467a0d38eeaef98e
chenliujin <liujin.chen@qq.com>
2015-05-30 12:27:33 +0800

    update

--- thinkphp/model/countries.php
+++ thinkphp/model/countries.php
@@ -26,13 +26,22 @@ class countries extends Model
 	 */
 	public function getAll()
 	{
+		static $data;
+		
+		if (!empty($data)) {
+			return $data;
+		}
 		$sql = "
 			SELECT *
 			FROM " . self::GetTableName() . "
 			";
 		$result = $this->query($sql, array(), __CLASS__);
+		
+		foreach ($result as $obj) {
+			$data[$obj->countries_iso_code_2] = $obj;
+		}
 
-		return $result;
+		return $data;
 	}
 
 	/**
@@ -49,4 +58,38 @@ class countries extends Model
 
 		return $data;
 	}
+	
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2015-05-30
+	 */
+	static public function getContinents($code)
+	{
+		static $countries;
+		
+		if (empty($countries)) {
+			$class = __CLASS__;
+			$_this = new $class;
+			$countries = $_this->getAll();
+		}
+		
+		return $countries[$code]->continents;
+	}
+	
+	/**
+	 * @author chenliujin <liujin.chen@qq.com>
+	 * @since 2015-05-30
+	 */
+	static public function getCountriesNameCN($code)
+	{
+		static $countries;
+		
+		if (empty($countries)) {
+			$class = __CLASS__;
+			$_this = new $class;
+			$countries = $_this->getAll();
+		}
+		
+		return $countries[$code]->countries_name_cn;	
+	}	
 }
