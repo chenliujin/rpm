d4d8c661599038f037de6a634c3fe6c861ea5703
chenliujin <liujin.chen@qq.com>
2015-08-14 18:24:12 +0800

    update

--- BI/PHP/php_error_log.sh
+++ BI/PHP/php_error_log.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# log sample:
-# [24-Jul-2015 00:00:31 PRC] PHP Warning:  chmod(): No such file or directory in /ssd/www/zencart/website/v20131028/includes/modules/pages/popup_images_upload/header_php.php on line 210
+year=`date +%Y`
+month=`date +%m`
+day=`date +%d`
 
-cat 'err_log_2015 07 24.log' | \
-sed ':a;N;$!ba;s/\n//g' | \
-sed 's/[[:space:]]\{2,\}//g' | \
-sed 's/\(\[[0-9]\{2\}\)/\n\1/g' | \
-grep -v '/usr/lib/php/pear/cacheHubWeb' | \
-grep -v 'Cache is null' |
-grep -v 'Client ip' | \
-awk -F ']' '{print $2,$NF}' \
-> a
+for ip in 192.168.0.10 192.168.0.11 192.168.0.12
+do
+    file=`ls /data/rawlog/log/php_error_log/$ip/$year$month/err_log_*$year*$month*$day*.log`
+    dest="/data/rawlog/log/php_error_log_stats/$year$month/$year$month$day/"
+
+    mkdir -p $dest
+    #cat "$file" | sed ':a;N;$!ba;s/\n//g' | sed 's/\(\[[0-9]\{2\}\)/\n\1/g' > $dest
+    cat "$file" | \
+    sed ':a;N;$!ba;s/\n//g' | \
+    sed 's/\(\[[0-9]\{2\}-\)/\n\1/g' | \
+    sed 's/[[:space:]]\{2,\}/ /g' | \
+    grep -v 'Cache is null' | \
+    grep -v 'Client ip' | \
+    awk -F ']' '{print $2,$NF}' > $dest/$ip'.log'
+done
