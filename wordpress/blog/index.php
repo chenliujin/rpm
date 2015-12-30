7617f3c2647224a2efaa71fc1a5c9753fbd1d725
chenliujin <liujin.chen@qq.com>
2015-12-30 23:06:37 +0800

    添加百度统计

--- wordpress/blog/index.php
+++ wordpress/blog/index.php
@@ -15,3 +15,14 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+?>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?89a560c052b711c526741fa15062141b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
